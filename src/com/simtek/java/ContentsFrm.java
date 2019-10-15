a9fe96a38bb8ee33188cf7e4942f1b1ab3bcbe7a
simtek2018 <49326376+simtek2018@users.noreply.github.com>
2019-10-15 17:23:15 -0400

    Added AddNumBtn

--- src/com/simtek/java/ContentsFrm.java
+++ src/com/simtek/java/ContentsFrm.java
@@ -1,10 +1,41 @@
 package com.simtek.java;
 
-import javax.swing.JFrame;
+import java.awt.BorderLayout;
+import java.awt.Container;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class ContentsFrm extends JFrame {
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EtchedBorder;
+
+public class ContentsFrm extends JFrame implements ActionListener {
+	private JButton addNumBtn;
+	
 	
 	public ContentsFrm() {
+		// Create Panels
+		Container c = getContentPane();
+		JPanel northPnl = new JPanel();
+		JPanel centerPnl = new JPanel();
+		EtchedBorder brdr = new EtchedBorder();
+		northPnl.setBorder(brdr);
+		centerPnl.setBorder(brdr);
+		
+		
+		// Create UI Components
+		addNumBtn = new JButton("Add Numbers");
+		
+		
+		// Add Listeners
+		addNumBtn.addActionListener(this);
+		
+		// Add components to panels
+		centerPnl.add(addNumBtn);
+		c.add(northPnl, BorderLayout.NORTH);
+		c.add(centerPnl, BorderLayout.CENTER);
+		
 		setSize(300, 300);
 		setVisible(true);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
@@ -13,6 +44,16 @@ public class ContentsFrm extends JFrame {
 	public static void main(String[] args) {
 		new ContentsFrm();
 
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		Object src = e.getSource();
+		
+		if (src == addNumBtn) {
+			System.out.println("Add Numbers!!");
+		}
+		
 	}
 
 }
