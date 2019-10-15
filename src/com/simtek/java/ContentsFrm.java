8b275133514d9874350526ab431d19d80b9bac6f
simtek2018 <49326376+simtek2018@users.noreply.github.com>
2019-10-15 16:59:26 -0400

    first commit

---
+++ src/com/simtek/java/ContentsFrm.java
@@ -0,0 +1,18 @@
+package com.simtek.java;
+
+import javax.swing.JFrame;
+
+public class ContentsFrm extends JFrame {
+	
+	public ContentsFrm() {
+		setSize(300, 300);
+		setVisible(true);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+	}
+
+	public static void main(String[] args) {
+		new ContentsFrm();
+
+	}
+
+}
